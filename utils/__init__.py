8ffb9572593d8588fef3eda3f1ea46f6f72604dc
Nemetonw <124575943+WeyaWtf@users.noreply.github.com>
2025-05-09 20:09:24 +0200

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+# mon_bot_social/utils/__init__.py
+# This file can be empty. It's used to mark the 'utils' directory as a Python package.
+
+# You could import key utility classes or functions if desired, e.g.:
+# from .config_manager import ConfigManager
+# from .logger import get_logger
+
+# But generally, keeping it empty is fine.
