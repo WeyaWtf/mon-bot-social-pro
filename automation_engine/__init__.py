8545e30870d84fe2d2fedbe269a9b3bc72d43df3
Nemetonw <124575943+WeyaWtf@users.noreply.github.com>
2025-05-09 20:07:18 +0200

    Create __init__.py

---
+++ automation_engine/__init__.py
@@ -0,0 +1,7 @@
+# mon_bot_social/automation_engine/__init__.py
+# This file can be empty. It's used to mark the 'automation_engine' directory as a Python package.
+
+# You could import key classes from submodules if you want them to be
+# accessible directly from the automation_engine package, e.g.:
+# from .browser_handler import BrowserHandler
+# from .element_selectors import ProfilePageLocators # etc.
