57efe327421d85b35899da236526a3263617018e
Nemetonw <124575943+WeyaWtf@users.noreply.github.com>
2025-05-09 20:10:59 +0200

    Create __init__.py

---
+++ automation_engine/actions/__init__.py
@@ -0,0 +1,22 @@
+# mon_bot_social/automation_engine/actions/__init__.py
+# This file makes the 'actions' directory a Python package.
+
+# You can optionally import all your action classes here to make them 
+# accessible more easily, e.g.:
+# from .follow_action import FollowAction
+# from .like_action import LikeAction
+# from .comment_action import CommentAction
+# from .unfollow_action import UnfollowAction
+# from .view_story_action import ViewStoryAction
+# from .gather_action import GatherAction
+# from .accept_follow_request_action import AcceptFollowRequestAction
+# from .direct_message_action import DirectMessageAction
+# from .check_new_followers_action import CheckNewFollowersAction
+
+# Then, from other parts of the project, you could do:
+# from automation_engine.actions import FollowAction
+# instead of:
+# from automation_engine.actions.follow_action import FollowAction
+
+# For now, keeping it empty is also perfectly fine and often preferred to avoid
+# circular dependencies if actions were to import from each other through this __init__.
